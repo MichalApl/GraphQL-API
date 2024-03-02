d59206cfe188b962e2a4bb67451085dad9849c24
MichalApl <123867268+MichalApl@users.noreply.github.com>
2024-03-02 19:55:00 +0200

    Add files via upload

---
+++ _db.js
@@ -0,0 +1,24 @@
+export let games = [
+    {id: '1', title: 'Witcher 3', platform: ['PC, PS5']},
+    {id: '2', title: 'Uncharted 4', platform: ['PS5']},
+    {id: '3', title: 'Horization Zero Dawn', platform: ['PS5']},
+    {id: '4', title: 'fheriodf', platform: ['dfsdfsd5']},
+    {id: '5', title: 'dsfsd 4', platform: ['dfsfs5']},
+]
+
+export let authors = [
+    {id: '1', name: 'Geralt', verified: true},
+    {id: '2', name: 'Yen', verified: false},
+    {id: '3', name: 'Ciri', verified: true},
+]
+
+export let reviews = [
+    {id: '1', rating: 10, content: 'lorem Ipsum', author_id: '2', game_id: '1'},
+    {id: '2', rating: 8, content: 'lorem Ipsum', author_id: '1', game_id: '2'},
+    {id: '3', rating: 9, content: 'lorem Ipsum', author_id: '3', game_id: '3'},
+    {id: '4', rating: 10, content: 'lorem Ipsum', author_id: '1', game_id: '4'},
+    {id: '5', rating: 10, content: 'lorem Ipsum', author_id: '3', game_id: '5'},
+    {id: '6', rating: 10, content: 'lorem Ipsum', author_id: '2', game_id: '2'},
+    {id: '7', rating: 10, content: 'lorem Ipsum', author_id: '1', game_id: '2'},
+
+]
